fb3d278b3752999e0b0125a6f6f601d82e670e0f
Ashrafi <ashrafikhanam.18@gmail.com>
2023-02-21 19:42:08 +0600

    class added

--- class.php
+++ class.php
@@ -1,1 +1,24 @@
-Class:
+<?php
+#Creating Classes, properties,methods--oop
+class Car{
+    private $color = 'red' ;
+    private $availableColor =['red', 'white', 'pink'];
+    public $weight;
+    private $year;
+
+    function setColor($color){
+       if(in_array($color, $this -> availableColor) ) {
+        $this ->color = $color;
+       }
+    }
+   function getColor(){
+       return $this -> color;
+    }
+}
+$ashrafisCar = new Car();//instances
+
+// $ashrafisCar -> color = "pink";//set or assign
+$ashrafisCar -> setColor('white');// set
+echo $ashrafisCar -> getColor();
+echo PHP_EOL;
+// echo $ashrafisCar -> color;
